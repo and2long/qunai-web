ae0c13a1c34e7bb76cbcc24b2df5c40aebaac1f8
Lilong Zhang <and2long@gmail.com>
2023-04-03 11:03:23 +0800

    Integrate antd and create DoctorListScreen.

---
+++ src/screens/DoctorListScreen.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
+export const DoctorListScreen = () => {
+
+  return (
+    <div>
+      <Title level={2}>医生列表</Title>
+    </div>
+  );
+}
