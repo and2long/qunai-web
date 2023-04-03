c3123993c8c34a31575881fc30e7ac464c5f07c9
Lilong Zhang <and2long@gmail.com>
2023-04-03 19:11:38 +0800

    Update env variable key name.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
 export const KEYCLOAK_REALM = "qunai";
 export const KEYCLOAK_CLIENT_ID = "qunai-medical";
-export const BASE_URL_KEYCLOAK = `${process.env.HOST_KEYCLOAK}:8080`;
-export const BASE_URL_BFF = `${process.env.HOST_BFF}:3001`;
+export const BASE_URL_KEYCLOAK = `${process.env.REACT_APP_HOST_KEYCLOAK}:8080`;
+export const BASE_URL_BFF = `http://${process.env.REACT_APP_HOST_BFF}:3001`;
