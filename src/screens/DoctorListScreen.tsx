f0fd7e385f0960a6df2bc8b70f962802b382b49b
Lilong Zhang <and2long@gmail.com>
2023-04-03 13:48:27 +0800

    lint:fix

--- src/screens/DoctorListScreen.tsx
+++ src/screens/DoctorListScreen.tsx
@@ -1,13 +1,21 @@
-import React, { useEffect } from "react";
-import { Typography } from 'antd';
+import { Typography } from "antd";
+import { useFetchDoctorList } from "../presenters/use-fetch-doctor-list";
+import { Doctor } from "../interfaces/doctor";
 
 const { Title } = Typography;
 
 export const DoctorListScreen = () => {
 
+  const { doctorList } = useFetchDoctorList();
+
   return (
     <div>
       <Title level={2}>医生列表</Title>
+      <ul>
+        {
+          doctorList.map((item: Doctor) => <li key={item.userId}>{item.userId}</li>)
+        }
+      </ul>
     </div>
   );
-}
+};
