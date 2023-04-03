9c775443d360e3ba3ede30e402fd16545f00e84c
Lilong Zhang <and2long@gmail.com>
2023-04-03 13:39:55 +0800

    Set env variables and constants.

---
+++ src/constants/constants.ts
@@ -0,0 +1,4 @@
+export const KEYCLOAK_REALM = "qunai";
+export const KEYCLOAK_CLIENT_ID = "qunai-medical";
+export const BASE_URL_KEYCLOAK = `${process.env.HOST_KEYCLOAK}:8080`;
+export const BASE_URL_BFF = `${process.env.HOST_BFF}:3001`;
